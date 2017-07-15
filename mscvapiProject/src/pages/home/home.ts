5b2f268f830c8e5901cb27009d10e2e8cdf0ce53
ggodreau <godreau@gmail.com>
2017-07-15 01:30:15 +0100

    refactored scope, added initPaper() function

--- mscvapiProject/src/pages/home/home.ts
+++ mscvapiProject/src/pages/home/home.ts
@@ -12,69 +12,38 @@ import paper from 'paper/dist/paper-full';
   templateUrl: 'home.html'
 })
 export class HomePage {
-    paperObj; //: any;
-    myTool;
-    path: any;
-    drag: any;
+//paperObj; //: any;
+  //  myTool;
+  //path: any;
 
   constructor(public navCtrl: NavController) {
   }
 
   ngAfterViewInit() {
-    this.paperObj = new paper.PaperScope();
-    this.paperObj.install(window);
-    this.paperObj.setup('myCanvas');
+    this.initPaper();
   }
 
-  
-  mouseDown(event){
-    var myTool = new this.paperObj.Tool();
+  initPaper(){
+    var paperObj = new paper.PaperScope();
+    paperObj.install(window);
+    paperObj.setup('myCanvas');
+
+    var myTool = new paperObj.Tool();
     myTool.activate();
 
-    var myPath = new this.paperObj.Path()
-    myPath.strokeColor = 'black';
-    console.log(myPath);
-    
-    // user 'this.paperObj' instead of 'paper'
-    //    var from = new this.paperObj.Point(20, 20);
-    //var to = new this.paperObj.Point(40, 40);
-    var rectPath = new this.paperObj.Path.Rectangle({
-        point: [20, 20],
-        size: [80, 80],
-        strokeColor: 'black'
-    });
-
+    //mouseDown(event){
+    //  var myPath = new this.paperObj.Path()
+    //  myPath.strokeColor = 'black';
+    //  console.log(myPath);
+      
     myTool.onMouseDown = function(event) {
-        //from.set(event.point.x, event.point.y);
-        //from.set(event.point.x, event.point.y);
-        //to.set(event.point.x, event.point.y);
-        //console.log('x:', event.point.x, 'y:', event.point.y);
-        console.log('mouseDown');
+          console.log('paperObj in nested:', paperObj);
     }     
-    myTool.onMouseDrag = function(event) {
-        //to.set(event.point.x, event.point.y); 
-        //console.log('x:', event.point.x, 'y:', event.point.y);
-        myPath.add(event.point);
-    }     
-    myTool.onMouseUp = function(event) {
-        //rectPath = this.paperObj.Path.Rectangle(from, to);
-        rectPath.set({
-            strokeColor: 'red',
-            strokeWidth: 10
-        });
-        console.log('mouseUp', 'rectPath: ', rectPath);
-    }
-    this.drag = true;
+      //this.myTool.onMouseDrag = function(event) {
+      //    var x = event.point.x;
+      //    var y = event.point.y;
+      //    console.log('x,y:', x, y);
+      //}
+     //}       
   }
-
-  //  mouseMove(){
-  //    if(this.drag){
-  //        console.log('mouse move');
-  //    }
-  //  }
-  //  mouseUp(){
-  //    console.log('mouse up');
-  //    this.drag = false;
-  //  }
-  
 }
